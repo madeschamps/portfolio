5310241aaddb0084ac4bdf5b81727c54fdd5384c
madeschamps <marc.andre.deschamps@gmail.com>
2024-02-06 21:41:46 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextConfig = {
    
 	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
 	// skipTrailingSlashRedirect: true,
-   
 	// Optional: Change the output directory `out` -> `dist`
 	// distDir: 'dist',
   }
