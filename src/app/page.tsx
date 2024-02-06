87cee8040b4023e3738e5fa5d8b76fe01ec95a53
madeschamps <marc.andre.deschamps@gmail.com>
2024-02-06 20:58:12 +0000

    Update escape chars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,19 @@ export default function Home() {
   return (
     <main className="p-24">
       <h1 className="text-5xl md:text-8xl">Marc Portoflio</h1>
-      <p>Web programmer</p>
+      <p>Web programmer </p>
       
       <h2>Salut! Hola! Ciao! Olá! Hello!</h2>
       <p>Since 2016</p>
 
       <p className="text-4xl mb-10">
-      ✨ Extensive Experience: With a journey that spans back to 2016, I've successfully navigated the dynamic landscape of WordPress development.
+      ✨ Extensive Experience: With a journey that spans back to 2016, I&#39;ve successfully navigated the dynamic landscape of WordPress development.
       </p>
 
 
       <div className="mb-10">
         <h2>A passion for not only programming languages</h2>
-        <p> Fluent in French, Spanish, Italian, Portuguese, and English, my language proficiency is more than just a skill; it's a result of my enriching travel journey.</p>
+        <p> Fluent in French, Spanish, Italian, Portuguese, and English, my language proficiency is more than just a skill; it&#39;s a result of my enriching travel journey.</p>
       </div>
 
       <div className="mb-10">
@@ -29,8 +29,8 @@ export default function Home() {
 
       <p className="text-4xl mb-10">
         Hire Me for Your Project!
-        Ready to elevate your online presence? Let's collaborate on your project and create something extraordinary together. From concept to execution, I bring expertise, passion, and a dedication to excellence.
-        Get in touch, and let's make your project a digital masterpiece!
+        Ready to elevate your online presence? Let&#39;s collaborate on your project and create something extraordinary together. From concept to execution, I bring expertise, passion, and a dedication to excellence.
+        Get in touch, and let&#39;s make your project a digital masterpiece!
       </p>
     </main>
   );
