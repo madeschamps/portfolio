3cb95ec5c6e1ba5b24124819d8762c5e03200a62
madeschamps <marc.andre.deschamps@gmail.com>
2024-02-06 20:15:16 +0000

    init NextJS project

---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-24">
+      <h1 className="text-5xl md:text-8xl">Marc Portoflio</h1>
+      <p>Web programmer</p>
+      
+      <h2>Salut! Hola! Ciao! Olá! Hello!</h2>
+      <p>Since 2016</p>
+
+      <p className="text-4xl mb-10">
+      ✨ Extensive Experience: With a journey that spans back to 2016, I've successfully navigated the dynamic landscape of WordPress development.
+      </p>
+
+
+      <div className="mb-10">
+        <h2>A passion for not only programming languages</h2>
+        <p> Fluent in French, Spanish, Italian, Portuguese, and English, my language proficiency is more than just a skill; it's a result of my enriching travel journey.</p>
+      </div>
+
+      <div className="mb-10">
+        <p>WordPress, React, NextJS</p>
+        <ul>
+          <li>✨ Quality-Driven: I believe in delivering nothing but the best. My commitment to quality ensures that every project meets high standards.</li>
+          <li>🤝 Team Player: Collaboration is key to success. I excel in team environments, leveraging collective skills for outstanding outcomes.</li>
+        </ul>
+      </div>
+
+      <p className="text-4xl mb-10">
+        Hire Me for Your Project!
+        Ready to elevate your online presence? Let's collaborate on your project and create something extraordinary together. From concept to execution, I bring expertise, passion, and a dedication to excellence.
+        Get in touch, and let's make your project a digital masterpiece!
+      </p>
+    </main>
+  );
+}
