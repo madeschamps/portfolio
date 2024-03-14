77485329d1f7e7f9b9cafe30b7aa90118959f228
madeschamps <marc.andre.deschamps@gmail.com>
2024-03-14 23:54:30 +0000

    Update

--- src/app/components/Preloader.tsx
+++ src/app/components/Preloader.tsx
@@ -7,62 +7,62 @@ const words = ["Hello", "Olà", "Ciao", "Bonjour", "Hola"]
 
 export default function Preloader() {
     const [index, setIndex] = useState(0);
-    const [dimension, setDimension] = useState({width: 0, height:0});
+    const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
-    useEffect( () => {
-        setDimension({width: window.innerWidth, height: window.innerHeight})
+    useEffect(() => {
+        setDimension({ width: window.innerWidth, height: window.innerHeight })
     }, [])
 
-    useEffect( () => {
-        if(index == words.length - 1) return;
-        setTimeout( () => {
+    useEffect(() => {
+        if (index == words.length - 1) return;
+        setTimeout(() => {
             setIndex(index + 1)
         }, index == 0 ? 1000 : 150)
     }, [index])
 
-    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
-    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`
+    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width / 2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
+    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`
 
     const curve = {
         initial: {
             d: initialPath,
-            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1]}
+            transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] }
         },
         exit: {
             d: targetPath,
-            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3}
+            transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 }
         }
     }
 
-	const opacity = {
-		initial: {
-			opacity: 0
-		},
-		enter: {
-			opacity: 0.75,
-			transition: {duration: 1, delay: 0.2}
-		},
-	}
-	
-	const slideUp = {
-		initial: {
-			top: 0
-		},
-		exit: {
-			top: "-100vh",
-			transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2}
-		}
-	}
+    const opacity = {
+        initial: {
+            opacity: 0
+        },
+        enter: {
+            opacity: 0.75,
+            transition: { duration: 1, delay: 0.2 }
+        },
+    }
+
+    const slideUp = {
+        initial: {
+            top: 0
+        },
+        exit: {
+            top: "-100vh",
+            transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
+        }
+    }
 
     return (
         <motion.div variants={slideUp} initial="initial" exit="exit" className={styles.introduction}>
-            {dimension.width > 0 && 
-            <>
-				<motion.p className={styles.text} variants={opacity} initial="initial" animate="enter">{words[index]}</motion.p>
-                <svg>
-                    <motion.path variants={curve} initial="initial" exit="exit"></motion.path>
-                </svg>
-            </>
+            {dimension.width > 0 &&
+                <>
+                    <motion.p className={styles.text} variants={opacity} initial="initial" animate="enter">{words[index]}</motion.p>
+                    <svg>
+                        <motion.path variants={curve} initial="initial" exit="exit"></motion.path>
+                    </svg>
+                </>
             }
         </motion.div>
     )
