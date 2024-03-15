fd217b42b66614d983f7838e9ea5771f90b03bb9
madeschamps <marc.andre.deschamps@gmail.com>
2024-03-15 10:48:34 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
 	output: 'export',
-};
-/* module.exports = nextConfig*/
-export default nextConfig;
+	images: {
+		unoptimized: true
+	}
+}
+
+module.exports = nextConfig
